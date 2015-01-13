e4b3816278f4d233919489c73a644a99a1b5e675
aHyunjaePark <hyunjae.park@access-company.com>
2015-01-13 15:57:51 +0000

    active record callback hell

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,82 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    test_block_proc_lambda
+    @users = User.all
+    respond_with(@users)
+  end
+
+  def show
+    respond_with(@user)
+  end
+
+  def new
+    @user = User.new
+    respond_with(@user)
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.name = "touched once!"
+    @user.name = "touched twice!"
+    @user.save
+    # Park.new.save
+    respond_with(@user)
+  end
+
+  def update
+    @user.update(user_params)
+    respond_with(@user)
+  end
+
+  def destroy
+    @user.destroy
+    respond_with(@user)
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+
+
+  def do_something(my_callback, *params)
+    # send ("my_callback", params)
+    # my_callback
+    # logger.debug "somesomesome"
+  end
+
+  def my_callback (*params)
+    logger.debug "calcalcalcal", params.inspect
+  end
+
+  # if you want to send callbacks, it MUST have to be a block
+  def my_block(&block)
+    block.call "hola!"
+    # yield "hola!" #alias for block.call
+  end
+
+  def test_block_proc_lambda
+    #important!! lambda parameter is with parentheses!!!!!!!!
+    hihi = ->(msg) { logger.debug "made by stabby lambda #{msg}" }
+    #important!! but proc is inside block!!!! awkward!!!
+    hihi2 = Proc.new { |msg| logger.debug "made by proc #{msg}" }
+    my_block(&hihi) #convert lambda to block with ampersand
+    my_block(&hihi2) #convert proc to block with ampersand
+    my_block do |msg| #multiline block
+      logger.debug "multiline block#{msg}"
+    end
+    my_block { logger.debug "singleline block" } #singleline block
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true
+  after_initialize -> { logger.debug 'after initialize' }
+  # 의문점: 람다나 proc을 파라미터로 전달을 하러면 & 를 꼭 붙여야 된다고 배웠다. 하지만 어떻게 안붙이고도 호출이 되는지....?
+  # 재밌는점은 붙이던 안붙이던 다 된다는거다...하지만 내 custom method는 안된다는거....
+  # core 에서 어떤 처리를 한것인가...
+  before_validation :call_this_before, &-> { logger.debug 'before validate 2222222222222222222222' }
+  after_validation &-> { logger.debug 'after validate' }
+  around_save do |activity, block|
+    logger.debug "before around"
+    block.call
+    logger.debug "after around"
+  end
+  before_save do
+    logger.debug 'before save'
+  end
+  before_update -> { logger.debug 'before_update' }
+  before_create lambda { logger.debug 'before_create' }
+  # around_create -> { logger.debug 'around_create' }
+  after_create -> { logger.debug 'after_create' }
+  after_update -> { logger.debug 'after_update' }
+  after_save -> { logger.debug 'after_save' }
+  after_commit -> { logger.debug 'after_commit' }
+  after_rollback -> { logger.debug 'after_rollback' }
+  after_find -> { logger.debug 'after find' }
+  after_touch -> { logger.debug 'after touch' }
+
+  private
+  def call_this_before
+    logger.debug 'before validate'
+  end
+
+  def some_around
+    logger.debug "before around"
+    yield
+    logger.debug "after around"
+  end
+end
